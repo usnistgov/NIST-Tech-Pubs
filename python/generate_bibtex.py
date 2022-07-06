1a9a458a84c9f6fb91682189222199e917a6ef99
Simson Garfinkel <simsong@acm.org>
2022-07-06 14:12:53 -0400

    generates bibtex

--- python/generate_bibtex.py
+++ python/generate_bibtex.py
@@ -2,31 +2,82 @@ import sys
 import xml.etree.ElementTree as ET
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
+import json
 
+class NoDOIException( Exception ):
+    pass
+
+
+## Translate XML tag to BiBTeX tag
 TAG_XLATE = [('title', 'title'),
-             ('subtitle', 'subtitle')
+             ('subtitle', 'subtitle'),
+             ('doi', 'doi'),
+             ('year', 'year'),
+             ('institution_name', 'institution'),
+             ('publisher_name', 'publisher'),
+             ('publisher_place', 'address')
              ]
+
+def show(obj):
+    ET.indent(obj)
+    print(ET.tostring(obj).decode('utf-8'))
+
+def get_text(node, tag):
+    try:
+        return node.find(f".//{tag}").text
+    except AttributeError:
+        return ""
 
 def xml_to_dict(node):
     """Returns a dict from the XML"""
     ret = dict()
+    ret['ENTRYTYPE'] = 'misc'
+
+    ## Do the simple maps
     for (xml_tag, bibtex_tag) in TAG_XLATE:
-        tags = node.findall(f".//{xml_tag}")
-        if tags:
-            ret[bibtex_tag] = tags[0].text
+        tag = node.find(f".//{xml_tag}")
+        if tag is not None and tag.text is not None:
+            ret[bibtex_tag] = tag.text
+
+    ## Get authors or editors
+    author = ""
+    for person in node.find(".//contributors").findall("person_name"):
+        name = (get_text(person, "given_name") + " " + get_text(person,"surname")).strip()
+        if author:
+            author += " and "
+        author += name
+    ret['author'] = author
+
+    try:
+        ret['ID'] = ret['doi']
+    except KeyError:
+        print("No DOI in:")
+        show(node)
+        raise NoDOIException()
+
     return ret
 
 
-def generate(in_fname,out_fname):
+def generate(in_fname, out_fname, doi=None):
+    db = BibDatabase()
     tree = ET.parse(in_fname)
+    if doi:
+        obj = tree.find(f'.//query/doi[. = "{doi}"]/..')
+        if not obj:
+            raise KeyError(doi)
+        print(json.dumps(xml_to_dict(obj),indent=4))
+        return
+
     for node in tree.findall(".//doi_record"):
-        t = node.findall(".//title")
-        if(len(t)==1):
-            continue
-        print("Total number of titles:",len(t))
-        for _ in t:
-            print(_.text)
-        print("")
+        try:
+            db.entries.append(xml_to_dict(node))
+        except NoDOIException:
+            pass
+
+    writer = BibTexWriter()
+    with open(out_fname,"w") as out:
+        out.write(writer.write(db))
+    return
 
 if __name__=="__main__":
     import argparse
@@ -34,5 +85,6 @@ if __name__=="__main__":
                                 description="Generate BiBTeX file from NIST XML")
     a.add_argument("infile", help="Input file")
     a.add_argument("outfile", help="Output file")
+    a.add_argument("--doi", help="Only generate output for this doi")
     args = a.parse_args()
-    generate(args.infile, args.outfile)
+    generate(args.infile, args.outfile, doi=args.doi)
